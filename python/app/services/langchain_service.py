1f8427fad4836a18778f682ec64064d9db819f49
古谷竜一 <campbel@campbel.local>
2024-01-17 22:04:07 +0900

    Commit at 2024-01-17 22:04:07

--- python/app/services/langchain_service.py
+++ python/app/services/langchain_service.py
@@ -3,6 +3,8 @@ from typing import List, Optional, Union
 import tiktoken
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
+from config import openai
+from config import openai as openai_config
 
 from config import settings
 
@@ -15,6 +17,10 @@ class Langchain:
         temperature: float = 0.7,
         max_tokens: int = 1000,
     ) -> Optional[str]:
+        useable_model_names = [model.value for model in openai_config.UseableModelName]
+        if max_tokens not in useable_model_names:
+            raise ValueError("max_tokens is invalid.")
+
         langchain_messages = self._create_messages(messages)
 
         if self._check_tokens(langchain_messages, model_name) is False:
@@ -31,36 +37,6 @@ class Langchain:
         result = chat.invoke(langchain_messages)
         return result.content
 
-    def _max_tokens_by_model_name(self, model_name: str) -> Optional[int]:
-        if model_name == "gpt-4-1106-preview":
-            return 128000
-
-        if model_name == "gpt-4":
-            return 8192
-
-        if model_name == "gpt-4-32k":
-            return 32768
-
-        if model_name == "gpt-3.5-turbo-1106":
-            return 16385
-
-        return None
-
-    def _encode_name_by_model_name(self, model_name: str) -> Optional[str]:
-        if model_name == "gpt-4-1106-preview":
-            return "cl100k_base"
-
-        if model_name == "gpt-4":
-            return "cl100k_base"
-
-        if model_name == "gpt-4-32k":
-            return "cl100k_base"
-
-        if model_name == "gpt-3.5-turbo-1106":
-            return "cl100k_base"
-
-        return "cl100k_base"
-
     def _check_tokens(self, langchain_messages: list, model_name: str) -> bool:
         """
         token数のチェック
@@ -68,7 +44,7 @@ class Langchain:
         if len(langchain_messages) == 0:
             return False
 
-        encode_name = self._encode_name_by_model_name(model_name)
+        encode_name = openai.ENCODE_NAME[model_name]
 
         tiktoken_enc = tiktoken.get_encoding(encode_name)
 
@@ -77,7 +53,7 @@ class Langchain:
             tokens = tiktoken_enc.encode(langchain_message.content)
             total_tokens += len(tokens)
 
-        return self._max_tokens_by_model_name(model_name) > total_tokens
+        return openai.MAX_TOKEN[model_name] > total_tokens
 
     def _create_messages(
         self, messages: list
