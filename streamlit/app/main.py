341e0fada9dd1ad9f021b65336a172dca87cf4ed
古谷竜一 <campbel@campbel.local>
2024-01-24 14:42:48 +0900

    Commit at 2024-01-24 14:42:48

--- streamlit/app/main.py
+++ streamlit/app/main.py
@@ -1,4 +1,23 @@
 import streamlit as st
+from app.services.sentiment_service import SentimentService
 
-st.title("My First Streamlit App")
-st.write("Welcome to my app!")
+# Streamlitアプリのタイトル
+st.title("感情分析アプリ")
+
+# ユーザーにテキスト入力を求める
+user_input = st.text_area("ここにテキストを入力してください", "")
+
+# 感情分析
+if user_input:
+    result = SentimentService().sentiment_sentences([user_input])[0]
+
+    print(result)
+
+    # # 分析結果の表示
+    # st.write("### 分析結果")
+    # if sentiment > 0:
+    #     st.write(f"ポジティブな感情 ({sentiment:.2f})")
+    # elif sentiment < 0:
+    #     st.write(f"ネガティブな感情 ({sentiment:.2f})")
+    # else:
+    #     st.write("中立的な感情")
---
+++ streamlit/app/services/transformer.py
@@ -0,0 +1,44 @@
+from typing import Any, Dict, List
+
+from sentence_transformers import SentenceTransformer
+from transformers import pipeline
+
+
+class SummaryService:
+    def summary_sentences(self, sentences: List[str]) -> List[str]:
+        # 日本語のテキスト要約モデルをロードします。
+        summarizer = pipeline("summarization", model="sonoisa/t5-base-japanese")
+
+        # 要約を生成します。ここでは長い日本語のテキストを例としています。
+        summaries = summarizer(sentences, min_length=1, max_length=1000)
+
+        return [summary["summary_text"] for summary in summaries]
+
+
+class EmbeddingService:
+    def embedding_sentences(self, sentences: List[str]) -> List[List[float]]:
+        # 1. cl-tohoku/bert-base-japanese-whole-word-masking
+        # このモデルは全単語マスキングを行ったBERTモデルで、単語レベルでの意味を捉えることができます。
+        # したがって、単語の意味が重要なタスク（例えば、文章の意味理解や質問応答など）に適しています。
+        #
+        # 2. cl-tohoku/bert-base-japanese-char
+        # このモデルは文字レベルでBERTモデルを訓練したもので、
+        # 細かい文字レベルの情報を必要とするタスク（例えば、品詞タグ付けや固有表現抽出など）に適しています。
+
+        model = SentenceTransformer("cl-tohoku/bert-base-japanese-whole-word-masking")
+        # model = SentenceTransformer("cl-tohoku/bert-base-japanese-whole-word-masking")
+        sentence_embeddings = model.encode(sentences)
+
+        return [
+            sentence_embedding.tolist() for sentence_embedding in sentence_embeddings
+        ]
+
+
+class SentimentService:
+    def sentiment_sentences(self, sentences: List[str]) -> List[Dict[str, Any]]:
+        model_name = "lxyuan/distilbert-base-multilingual-cased-sentiments-student"
+        classifier = pipeline("sentiment-analysis", model=model_name)
+
+        classifier_results = classifier(sentences)
+
+        return classifier_results
